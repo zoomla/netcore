02c3fc223633c2dc6efa811e6ac4c790453e8ce2
zoomla <web@hx008.com>
2020-05-30 14:30:25 +0800

    合并1~这是发哥自动提交机器人-来自Zoomla!逐浪CMS,中国最优web服务商|CMS|高端逐浪字库www.z01.com

---
+++ 001Mvc_SystemIO/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC.Models;
+using System.IO;
+using System.Data;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //DriveInfo[] drivers = DriveInfo.GetDrives();
+            //ViewData["message"] = DriveInfo.GetDrives();
+            DriveInfo[] drivers = DriveInfo.GetDrives();
+            DataTable data = new DataTable();
+            data.Columns.Add("DriverName", typeof(string));
+            data.Columns.Add("DriveType", typeof(string));
+            data.Columns.Add("DriveFormat", typeof(string));
+            data.Columns.Add("VolumeLabel", typeof(string));
+            data.Columns.Add("TotalFreeSpace", typeof(long));
+            data.Columns.Add("TotalSize", typeof(long));
+            data.Columns.Add("Percent", typeof(float));
+            data.Columns.Add("AvailableFreeSpace", typeof(float));
+            foreach (DriveInfo info in drivers)
+            {
+                //这里仅列出硬盘分区的使用情况
+                if (info.DriveType == DriveType.Fixed)
+                {
+                    DataRow row = data.NewRow();
+                    //磁盘分区名称
+                    row["DriverName"] = info.Name;
+                    //磁盘分区类型，如软驱、硬盘或者光驱等
+                    row["DriveType"] = info.DriveType;
+                    //磁盘分区文件系统，如FAT16、FAT32及NTFS等
+                    row["DriveFormat"] = info.DriveFormat;
+                    //磁盘分区卷标
+                    row["VolumeLabel"] = info.VolumeLabel;
+                    //磁盘空闲容量
+                    row["TotalFreeSpace"] = info.TotalFreeSpace / 1024 / 1024 / 1024;
+                    //磁盘容量
+                    row["TotalSize"] = info.TotalSize / 1024 / 1024 /1024;
+                    //磁盘使用量百分比
+                    row["Percent"] = info.TotalFreeSpace * 100 / info.TotalSize;
+                    //当前用户的磁盘配额
+                    row["AvailableFreeSpace"] = info.AvailableFreeSpace / 1024 / 1024 / 1024;
+                    data.Rows.Add(row);
+                    //DataRow["DriverName"] = info.Name;
+                    //DataRow["DriveType"] = info.TotalSize;
+                    //DataRow["DriveFormat"] = info.DriveFormat;
+                }
+            }
+            ViewBag.abcd = data;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,67 @@
+
+[目录]
+
+<!-- TOC -->
+
+- [Zoomla!逐浪CMS经典dotNET core项目开发实例库](#zoomla逐浪cms经典dotnet-core项目开发实例库)
+    - [仓库目录](#仓库目录)
+    - [基本环境](#基本环境)
+    - [本项目介绍](#本项目介绍)
+    - [开放性说明](#开放性说明)
+    - [获取支持](#获取支持)
+
+<!-- /TOC -->
+
+![Zoomla!逐浪软件](源码与接口使用说明/logo.svg)
+# Zoomla!逐浪CMS经典dotNET core项目开发实例库
+
+## 仓库目录
+| 序号 | 目录  | 说明 |
+| ------------ | ------------ | ------------ |
+|  001 |  001Mvc_SystemIO | 基于MVC模式读取SystemIO类，获取硬盘信息 |
+|   |   |   |
+
+## 基本环境
+
+基于 `.net core`开发库
+最低版本:`.net core3.0`
+
+
+Zoomla!逐浪CMS：中文业界alexa排名第一的CMS系统|专注.net与windows平台企业级研发，集成内容管理、webfont、商城、店铺、黄页、教育、考试、3D、三维全景、混合现实、CRM、ERP、OA、论坛、贴吧等为一体，打造国内高端的CMS产品典范。
+
+官网：www.z01.com
+
+免费下载：www.z01.com/mb
+
+视频教程：www.z01.com/mtv
+
+模板资源：www.z01.com/mb
+
+
+QQ交流群号：
+[![加入QQ群](https://img.shields.io/badge/一群-541450128-blue.svg?style=for-the-badge&logo=appveyor)](https://jq.qq.com/?_wv=1027&k=5Ephzpq)   [![加入QQ群](https://img.shields.io/badge/二群-601781959-blue.svg?style=for-the-badge&logo=appveyor)](https://jq.qq.com/?_wv=1027&k=50a28BK) 
+
+
+官方QQ客服：
+[![官方QQ客服1](https://img.shields.io/badge/官方QQ客服1-524979923-red.svg?style=for-the-badge&logo=appveyor)](http://wpa.qq.com/msgrd?v=3&uin=745151353&site=qq&menu=yes)  [![官方QQ客服2](https://img.shields.io/badge/官方QQ客服2-1799661890-red.svg?style=for-the-badge&logo=appveyor)](http://wpa.qq.com/msgrd?v=3&uin=1799661890&site=qq&menu=yes) 
+
+## 本项目介绍
+
+秉承Zoomla!逐浪CMS开放开源精神，提供优秀软件品。
+
+## 开放性说明
+本项目基于Zoomla!逐浪CMS技术栈结合开发。
+有道是：
+
+- 建网站,选逐浪。
+- 易上手,功能棒。
+- 质量好,服务善。
+- 高性能,最划算。
+
+
+
+## 获取支持
+产品官网：http://www.z01.com
+
+使用手册：http://help.z01.com
+
